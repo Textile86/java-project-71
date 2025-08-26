8f4f7110472ea83d2851c0acd9d8d80011fe7173
textile86 <textile2006@list.ru>
2025-08-26 19:38:38 +0300

    step 9-2

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -22,29 +22,4 @@ public class Differ {
         return Formatter.format(data1, data2, format);
     }
 
-    public static String generateDiff(Map<String, Object> data1, Map<String, Object> data2) {
-        StringBuilder result = new StringBuilder("{\n");
-
-        TreeSet<String> allSortedKeys = new TreeSet<>();
-        allSortedKeys.addAll(data1.keySet());
-        allSortedKeys.addAll(data2.keySet());
-
-        for (String key : allSortedKeys) {
-            if (data1.containsKey(key) && !data2.containsKey(key)) {
-                result.append("  - ").append(key).append(": ").append(data1.get(key)).append("\n");
-            } else if (!data1.containsKey(key) && data2.containsKey(key)) {
-                result.append("  + ").append(key).append(": ").append(data2.get(key)).append("\n");
-            } else if (data1.get(key) != null && data1.get(key).equals(data2.get(key))) {
-                result.append("    ").append(key).append(": ").append(data1.get(key)).append("\n");
-            } else if (data1.get(key) == null && data2.get(key) == null) {
-                result.append("    ").append(key).append(": null\n");
-            } else {
-                result.append("  - ").append(key).append(": ").append(data1.get(key)).append("\n");
-                result.append("  + ").append(key).append(": ").append(data2.get(key)).append("\n");
-            }
-        }
-        result.append("}");
-
-        return result.toString();
-    }
 }
